fcb938f266766c81f18ae2a5e5eb35e631d83216
Mohamed Desouky <mohamed.ahm9833@gmail.com>
2021-02-08 17:18:59 +0700

    Google & Facebook Authentication is Working

--- src/App/Components/SocialLogin/SocialLogin.jsx
+++ src/App/Components/SocialLogin/SocialLogin.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
 
+import firebase from '../../firebase/Firebase.config';
+import { useDispatch } from 'react-redux';
+import {userLogin} from '../../Redux/Auth/AuthActions'
+
 import './StyleSocialLogin.css'
 
 export default function SocialLogin() {
 
+    const dispatch = useDispatch();
+    const socialLogin = async (selectedProvider) => {
+        let provider;
+
+        const auth = firebase.auth();
+        if (selectedProvider === "facebook") {
+            provider = new firebase.auth.FacebookAuthProvider();
+        }
+        if (selectedProvider === "google") {
+            provider = new firebase.auth.GoogleAuthProvider();
+        }
+        try {
+            const result = await auth.signInWithPopup(provider);
+            console.log(result.user);
+            dispatch(userLogin(result.user));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className='flex btn_group'>
-            <button className='social_btn flex_center'>
+            <button onClick={()=>socialLogin('facebook')}  className='social_btn flex_center'>
                 <img src={facebookLogo} alt='Facebook Login' />
                 Login with Facebook
             </button>
-            <button className='social_btn flex_center google_btn'>
+            <button onClick={()=>socialLogin('google')} className='social_btn flex_center google_btn'>
                 <img src={googleLogo} alt='Google Login' />
                 Login with Google
             </button>
